b5822157952549ac3034ab6c96364d5d2261f890
mstadt <mstadt@uwaterloo.ca>
2022-12-06 15:38:13 -0500

    model code finished

--- model_RosenzweigMacArthur.m
+++ model_RosenzweigMacArthur.m
@@ -34,14 +34,31 @@ tspan = [0,tend];
 %   These options may be adjusted when adjusting parameter values
 %   if users run into numerical issues.
 options = odeset('RelTol', 1.0e-6, 'AbsTol', 1.0e-9);
-fprintf('** solving ODEs **')
+fprintf('** solving ODE system ** \n')
 [t,x] = ode23s(@(t,x) RMfoodchainDeng(t,x,pars),tspan, x0, options);
+fprintf('** ODE system complete ** \n')
 
+%%------------------------------------
+%% Save Model Simulation Results
+%%------------------------------------
+savefile = input('Do you want to save the simulation results? (0 - no, 1 - yes)');
+if savefile
+   % user input a string for simulation results file name
+   notes = input('simulation notes: ');
+   save_fname = strcat(date, '_RMmodel_notes-', notes, '.mat');
+   % check to make sure the save file does not exist
+   if isfile(save_fname)
+       notes = input('WARNING! filename exists! change notes: ');
+       save_fname = strcat(date, '_RMmodel_notes-', notes,'.mat');
+   end
+   % save simulation results and simulation specifications
+   save(save_fname, 't', 'x', 'pars', 'x0', 'options', 'tspan');
+   fprintf('simulation results saved to: %s \n', save_fname)
+end
 
-%%-----------------------------------------------
-%% Model Function
-%%-----------------------------------------------
-
+%%------------------------------------
+%% Function used for model equations
+%%------------------------------------
 function dxdt = RMfoodchainDeng(t,x,pars)
 % NOTE: let x = x1, y = x2, z = x3 from equations written in Deng 2001
     x1 = x(1);
