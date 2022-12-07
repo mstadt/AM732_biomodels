dcef3c81b15e0b8f88372643341735a56a3a0131
agkunze <102260732+agkunze@users.noreply.github.com>
2022-12-07 10:10:20 -0500

    update postprocessing

--- postprocess_amelia.m
+++ postprocess_amelia.m
@@ -1,6 +1,6 @@
 clear all; close all;
 
-dat = load('06-Dec-2022_RMmodel_notes-delta2=0647401.mat');
+dat = load('06-Dec-2022_RMmodel_notes-delta2=062.mat');
 t = dat.t;
 x = dat.x;
 pars = dat.pars;
@@ -9,11 +9,13 @@ pars = dat.pars;
 lw = 1.0; % linewidth
 
 % start ID for after transient
-start_time = 1500;
-end_time   = 2000;
+start_time = 1000; %1500;
+end_time   = 2400;
 
 startID = find(t>start_time,1,'first');
 endID = find(t>end_time,1,'first');
+
+
 % make the figures
 figure(1) %to recreate the "period-doubling cascade" plots in Fig. 6
 betterplots
@@ -25,24 +27,38 @@ title(['\delta_2 = ' num2str(pars.delta2)])
 hold off
 
 % for figure 2
+% f=0 
 [X,Z] = meshgrid(0:0.01:1,0:0.01:0.3);
-Y = (pars.beta1+X).*(1-X); %f=0 
-%y=pars.delta2*pars.beta2/(1-pars.delta2); %z-nullcline of f=0?
+Y = (pars.beta1+X).*(1-X); 
 
-%gamma nullcline on f=0
+% z-nullcline on f=0
+xstar = (-pars.beta1*pars.delta2+pars.beta1+pars.delta2-...
+    sqrt((pars.delta2-1)*((pars.beta1^2+2*pars.beta1+4*pars.beta2+1)*pars.delta2...
+    -(pars.beta1+1)^2))-1)/(-2+2*pars.delta2); % constant value of x on z-nullcline
+ystar = pars.delta2*pars.beta2/(1-pars.delta2); % constant value of y on z-nullcline
+znull=linspace(0,0.3,100);
+xnull=xstar*ones(100,1);
+ynull=ystar*ones(100,1); 
+
+% gamma nullcline on f=0
 x0 = linspace(0, 1, 100);
 y0 = (pars.beta1+x0).*(1-x0);
 z0 = (pars.beta1*pars.delta1 + pars.delta1*x0 - x0).*(pars.beta1*x0 + x0.^2 - pars.beta1 - pars.beta2 - x0)./(pars.beta1 + x0); 
 
-figure(2) % to recreat Fig. 6a from Hek
+figure(2) % to recreate Fig. 6a from Hek
 betterplots
 plot3(x(startID:endID,3),x(startID:endID,1),x(startID:endID,2),'linewidth',lw) 
 hold on
 mesh(Z,X,Y)
 plot3(z0, x0, y0)
+plot3(znull, xnull, ynull, 'Color', 'red')
 xlabel('z')
 ylabel('x')
 zlabel('y')
 xlim([0 0.25])
 title(['\delta_2 = ' num2str(pars.delta2)])
 hold off
+
+
+
+
